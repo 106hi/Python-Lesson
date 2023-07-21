4235e63b2ed38195d4783430bf61872e2002f93e
106hi <hiro3428shs96kzsi@gmail.com>
2023-07-21 10:29:22 +0000

    Change exercises.py

--- exercises.py
+++ exercises.py
@@ -1,5 +1,3 @@
-li = [{'a': 6, 'b': 7, 'c': 6},
-      {'a': 4, 'b': 2, 'c': 3},
-      {'a': 1, 'b': 5, 'c': 8}]
-li_sorted = sorted(li, reverse=True, key=lambda x: x['b'])
-print(li_sorted)
+li = [5, 4, 3, 2, 1]
+ans = [idx + elem for idx, elem in enumerate(li)]
+print(ans)
