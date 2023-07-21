ec713d25a3c95acc4c9abffb7e8ddfd788804334
106hi <hiro3428shs96kzsi@gmail.com>
2023-07-21 10:08:27 +0000

    Change exercises.py

--- exercises.py
+++ exercises.py
@@ -1,2 +1,5 @@
-a = 'This is sentence .\n'
-print(a.strip())
+li = [{'a': 6, 'b': 7, 'c': 6},
+      {'a': 4, 'b': 2, 'c': 3},
+      {'a': 1, 'b': 5, 'c': 8}]
+li_sorted = sorted(li, reverse=True, key=lambda x: x['b'])
+print(li_sorted)
