23d3a86d275cb287fc8cc704b95aeb19c83692a1
106hi <hiro3428shs96kzsi@gmail.com>
2023-07-21 08:50:07 +0000

    Change exercises.py

--- exercises.py
+++ exercises.py
@@ -1,2 +1,4 @@
-d = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5}
-print(list(d.items()))
+d = {'apple': 10, 'grape': 20, 'orange': 30}
+d['apple'] = d.get('apple', -1)
+d['pinapple'] = d.get('pinapple', -1)
+print(d)
