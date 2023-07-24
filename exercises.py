dedd716102ad5bcd9c9c10c5eaddf247f8876f85
106hi <hiro3428shs96kzsi@gmail.com>
2023-07-24 06:40:01 +0000

    Fix exercises.py

--- exercises.py
+++ exercises.py
@@ -1,8 +1,5 @@
-li1 = list(range(1, 32))
-li2 = list(range(1,13))
-counter = 0
-for elem1 in li1:
-    for elem2 in li2:
-        if elem1%10 == elem2%10:
-            counter += 1
-print(counter)
+dic = {'two': 324, 'four': 830, 'three': 493, 'one': 172, 'five': 1024}
+items_list = list(dic.items())
+sorted_items_list = sorted(items_list, key = lambda x: x[1])
+ans = [elem[0] for elem in sorted_items_list]
+print(ans)
