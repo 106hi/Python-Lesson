702a80972679776fa8fc4195ebcd9480b83d6f47
106hi <hiro3428shs96kzsi@gmail.com>
2023-07-24 06:24:00 +0000

    Fix exercises.py

--- exercises.py
+++ exercises.py
@@ -1,4 +1,8 @@
-import math
-theta = math.pi/2
-ans = math.sin(theta)**2 + math.cos(theta)**2
-print(ans)
+li1 = list(range(1, 32))
+li2 = list(range(1,13))
+counter = 0
+for elem1 in li1:
+    for elem2 in li2:
+        if elem1%10 == elem2%10:
+            counter += 1
+print(counter)
