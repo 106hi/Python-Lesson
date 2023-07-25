6b0191fb1f689620386a9a4dc2fc83c8bb17f870
106hi <hiro3428shs96kzsi@gmail.com>
2023-07-25 10:05:39 +0000

    Add code to find the divisor in divisor.py

--- divisor.py
+++ divisor.py
@@ -1,1 +1,7 @@
-def divisor_list(num)
+def divisors_list(num):
+    divisors = []
+    for i in range(1, num + 1):
+        if num % i == 0:
+            divisors.append(i)
+    return divisors
+print(divisors_list(36))
